d003258e90f9ffac0e6369e3f91331e966f45a6a
IniyaK <iniyak129@gmail.com>
2024-01-18 11:51:08 +0530

    Add JaCoCo test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    jacoco
 }
 
 group = "org.example"
@@ -12,8 +13,44 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.9.0")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+
+    testLogging.events("passed", "skipped", "failed")
+    testLogging.exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    testLogging.info
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+
+    finalizedBy(tasks.jacocoTestCoverageVerification)
+
+    println(
+        "------------------------------------------------------------------------------------------------\n " +
+            "For test coverage get jacoco report at: build/reports/jacoco/test/html/index.html\n" +
+            "------------------------------------------------------------------------------------------------"
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            excludes = listOf("org.ik.Main")
+            limit {
+                minimum = "0.95".toBigDecimal()
+            }
+            element = "CLASS"
+        }
+    }
 }
--- src/test/java/org/ik/MainTest.java
+++
@@ -1,4 +0,0 @@
-package org.ik;
-
-public class MainTest {
-}
